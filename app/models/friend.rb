d0d30f36c35e1545f9dfbb6e3b201eb36e92d2af
Hamza Shaikh <hamza.shaikh@roxiler.com>
2024-08-26 13:54:21 +0530

    "adding validation fields in freinds form"

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,3 +1,14 @@
 class Friend < ApplicationRecord
     belongs_to :user
+
+    validates :first_name , presence: true ,  length: { minimum: 3, maximum: 15}
+    validates :last_name , presence: true , length: { minimum: 3, maximum: 15}
+    validates :email, 
+               uniqueness: { case_sensitive: false },
+               presence: true,
+               format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,   message: 'must be a valid email address' }    
+    validates :phone,
+               presence: true,
+               format: { with: /\A\d+\z/,  message: 'must only contain numbers' }
+    validates :twitter , presence: true , uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 15}
 end
