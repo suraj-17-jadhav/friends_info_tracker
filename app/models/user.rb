114f4a099afa8d7045407e9cdf834ba8eddb8070
Hamza Shaikh <hamza.shaikh@roxiler.com>
2024-08-26 14:05:30 +0530

    "adding user form validation"

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :friends
+  has_many :friends
+  validates :email, 
+            uniqueness: { case_sensitive: false },
+            presence: true,
+            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,   message: 'must be a valid email address' }  
+  validates :encrypted_password, 
+            presence: true,
+            length: { minimum: 6 , maximum: 15}
 end
